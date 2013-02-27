19c653c64302b5e0c6601ddeb154a66264655806
Eduardo Raad <eduardoraad@gmail.com>
2013-02-27 12:16:30 -0500

    test qr reader

---
+++ app.js
@@ -0,0 +1,18 @@
+function onLoad() {
+    document.addEventListener(
+      "deviceready", onDeviceReady, false);
+}
+
+function onDeviceReady() {
+    Lungo.dom('#qr-reader').on('load', function(event){
+        alert("qr");
+        window.plugins.barcodeScanner.scan( function(result) {
+            alert("We got a barcode\n" +
+              "Result: " + result.text + "\n" +
+              "Format: " + result.format + "\n" +
+                  "Cancelled: " + result.cancelled);
+        }, function(error) {
+            alert("Scanning failed: " + error);
+        });
+    });
+}
